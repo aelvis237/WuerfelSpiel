092b17ff5883c9e9106667769930698d080fe89a
aelvis237 <elvisashangawayi@gmail.com>
2023-12-18 20:35:36 +0100

    updated

--- src/Wuerfel.java
+++ src/Wuerfel.java
@@ -1,15 +1,12 @@
 import java.util.Random;
 
 public class Wuerfel {
-    // Klassenvariable für die maximale Augenzahl des Würfels
     private static int MAX_AUGENZAHL = 6;
 
-    // Statische Methode zum Initialisieren der MAX_AUGENZAHL
+
     public static void init(int n) {
         MAX_AUGENZAHL = n;
     }
-
-    // Statische Methode zum Würfeln einer Zahl zwischen 1 und MAX_AUGENZAHL
     public static int wuerfeln() {
         Random random = new Random();
         return random.nextInt(MAX_AUGENZAHL) + 1;
